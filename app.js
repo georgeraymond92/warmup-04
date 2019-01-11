730b4811f76d7cfcce106552467b1d6bc0ac554f
george <geomarray@gmail.com>
2019-01-10 22:53:05 -0800

    set up for new PR

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 'use strict';
-const util = require('util');
+
+
+// requireing superagent
 const superagent = require('superagent');
 
+
+// using promises
 function fetchPeopleWithPromises() {
-  // doo some things
 
   superagent.get('https://swapi.co/api/people/')
+
   .then(res => {
 
     let urlArr = [];
 
     for(let i = 0; i < res.body.results.length; i++){
+
       urlArr.push(res.body.results[i].url);
+
     }
+
     let promiseArr = [];
+
     for(let i = 0; i < urlArr.length; i++){
 
       promiseArr.push(superagent.get(urlArr[i]));
@@ -32,16 +40,23 @@ function fetchPeopleWithPromises() {
   })
 };
 
+// this is the asynch function 
+
 const fetchPeolpleWithAsync = async () => {
 
-  let peopleSet = await superagent.get('https://swapi.co/api/people/');
-  let people = (peopleSet.body && peopleSet.body.results) || [];
+
+  let peopleFromSwapi = await superagent.get('https://swapi.co/api/people/');
+  let people = (peopleFromSwapi.body && peopleFromSwapi.body.results) || [];
   let peopleRequests = people.map( (person) => {
+
     return superagent.get(person.url);
+
   });
+
   let swapiNames = await Promise.all(peopleRequests)
     .then( people => {
       let names = [];
+
       for(let data of people) {
         names.push(data.body.name);
       }
@@ -53,9 +68,9 @@ const fetchPeolpleWithAsync = async () => {
     return swapiNames;
 }
 
-// fetchPeolpleWithAsync()
-// .then( names =>  console.log('Async',names) );
 
-fetchPeopleWithPromises()
+fetchPeopleWithPromises();
+
+fetchPeolpleWithAsync();
 
 
